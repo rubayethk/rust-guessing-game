3af423f54d44e1c621dbd09e02b64fb802169725
Rubayeth Kamal <tamshi@gmail.com>
2015-08-16 23:15:33 -0400

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+//External references/libraries
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Error reading user input");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("You guessed too low."),
+            Ordering::Greater => println!("You guessed too high."),
+            Ordering::Equal => {
+                println!("You guessed correct. Win!!.");
+                break;
+            }
+        }
+    }
+}
